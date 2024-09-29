2ae7d48b1b3f2e8ef435877bc46aea61c83529e9
Mohammad Arshad <mohammadarshad01474@gmail.com>
2024-09-29 17:03:02 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,13 +5,13 @@ export default function Footer() {
   return (
     <footer
       className={clsx(
-        "fixed bottom-0 left-0 right-0",
+        "fixed bottom-0 left-0 right-0 -z-10",
         "block bg-black/5 py-2 px-3 text-sm/6 text-white",
         "border-t border-gray-200 dark:border-gray-700",
         "focus:outline-none data-[focus]:outline-2 data-[focus]:outline-offset-2 data-[focus]:outline-white/25"
       )}
     >
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8 -z-10">
         <div className="sm:flex sm:items-center sm:justify-between">
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
